98be057233d96c9a38d8daf0a212c3f90d752814
sych74 <sych74@gmail.com>
2024-01-30 14:50:24 +0100

    JE-68223

--- addons/db-recovery/scripts/db-recovery.js
+++ addons/db-recovery/scripts/db-recovery.js
@@ -25,13 +25,86 @@ function DBRecovery() {
         isRestore = false,
         envName = "env-3965472-db-1",
         config = {},
-        nodeManager;
+        envs = [],
+        nodeManager,
+        envManager;
 
     nodeManager = new nodeManager();
-
+    
+    
+    me.defineMultiregionEnvs = function(envName) {
+        
+        function getList(envName, items) {
+            var result = [],
+                index = 0,
+                itemData,
+                targetEnvNamePrefix;
+            
+            function dataExtractor(envName) {
+                var envNameSeparator = "-",
+                    index,
+                    tmp;
+                
+                tmp = envName.split(envNameSeparator);
+                index = tmp.pop();
+                
+                if (/\d+/.test(index)) {
+                    index = parseInt(index, 10);
+                } else {
+                    index = null;
+                }        
+                
+                return {
+                    envName: envName,
+                    envNamePrefix: tmp.join(envNameSeparator),
+                    index: index
+                };
+                
+            }
+            
+            function filter(data) {
+                return data.envNamePrefix === targetEnvNamePrefix &&
+                    typeof data.index === "number" && !isNaN(data.index);
+            }
+            
+            targetEnvNamePrefix = dataExtractor(envName).envNamePrefix;
+            
+            for(var i=0, item; item = items[i]; i++) {
+                itemData = dataExtractor(item.env.envName);
+                
+                if (filter(itemData)) {
+                    result.push(itemData.envName);
+                    index = Math.max(index, itemData.index);
+                }        
+            }
+            
+            return {
+                items       : result,
+                nextIndex   : index + 1
+            };
+        }
+        
+        var resp = jelastic.environment.control.GetEnvs(appid, session);
+        if (resp.result != 0) return resp;
+        
+        var data = getList(envName, resp.infos);
+        
+        return {
+            result: 0,
+            items: data.items,
+            nextIndex: data.nextIndex
+        };    
+    }
+        
+    
     me.process = function() {
         
-        let resp = me.defineRestore();
+        
+        let resp = me.defineMultiregionEnvs(envName);
+        return resp;
+        
+        
+        resp = me.defineRestore();
         if (resp.result != 0) return resp;
 
         resp = me.execRecovery();
@@ -98,8 +171,10 @@ function DBRecovery() {
         
         if (init) {
             me.setInitialize(true);
+            
             let resp = me.execRecovery();
             if (resp.result != 0) return resp;
+            
             me.setInitialize(false);
 
             resp = me.parseResponse(resp.responses);
@@ -581,6 +656,11 @@ function DBRecovery() {
             nodes: secondaries
         }
     };
+
+    function envManager() {
+        var me = this;
+        return me;
+    }
 
     function nodeManager() {
         var me = this,
